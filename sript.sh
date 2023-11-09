c4d81e916fba81bcb15c47facc17a4fa7e9b6da0
Burak Samed Doğu <burakdoguu1@gmail.com>
2023-11-09 20:03:09 +0300

    test

--- sript.sh
+++ sript.sh
@@ -2,6 +2,6 @@
 
 docker exec -it airflow /bin/bash
 
-docker exec airflow airflow tasks test data_aware_producer count_products_df 2023-01-01
+output=$(docker exec airflow airflow tasks test data_aware_producer count_products_df 2023-01-01)
 
 echo "$output"
